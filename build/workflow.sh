6cbf06648d5ed216ad10772a2a51af47ebdfb599
Jay Varner <jayvarner@gmail.com>
2023-09-12 09:32:06 -0400

    Fix build script

--- build/workflow.sh
+++ build/workflow.sh
@@ -39,9 +39,9 @@ sed \
 echo "Copied secret key over successfully"
 
 # GET the version of the project from the expath-pkg.xml
-# VERSION=$(cat expath-pkg.xml | grep package | grep version=  | awk -F'version="' '{ print $2 }' | awk -F'"' '{ print $1 }')
+VERSION=$(cat expath-pkg.xml | grep package | grep version=  | awk -F'version="' '{ print $2 }' | awk -F'"' '{ print $1 }')
 # GET the package name of the project from the expath-pkg.xml file
-# PACKAGE_NAME=$(cat expath-pkg.xml | grep package | grep version=  | awk -F'abbrev="' '{ print $2 }' | awk -F'"' '{ print tolower($1) }')
+PACKAGE_NAME=$(cat expath-pkg.xml | grep package | grep version=  | awk -F'abbrev="' '{ print $2 }' | awk -F'"' '{ print tolower($1) }')
 
 echo "Deploying app $PACKAGE_NAME:$VERSION"
 
